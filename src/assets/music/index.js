aaad1e0f219fff4ce609e0604a96d7f148ec956e
typosbro <typosbro@proton.me>
2023-08-24 20:05:23 +0900

    music assets

---
+++ src/assets/music/index.js
@@ -0,0 +1,100 @@
+// import karabako from "./karabako.mp3";
+// import miku from "./miku.mp3";
+// import marshmary from "./marshmary.mp3";
+// import rococo from "./rococo.mp3";
+// import sukima from "./sukima.mp3";
+
+// export default [karabako, miku, marshmary, rococo, sukima];
+
+// export const music = [
+//   {
+//     title: "Karabako",
+//     artist: "MIMI",
+//     cover: "https://i1.sndcdn.com/artworks-000136774690-7q1q1a-t500x500.jpg",
+//     file: karabako,
+//     duration: 204,
+//   },
+//   {
+//     title: "Miku",
+//     artist: "MIMI",
+//     cover: "https://i1.sndcdn.com/artworks-000136774690-7q1q1a-t500x500.jpg",
+//     file: miku,
+//     duration: 212,
+//   },
+//   {
+//     title: "Marshmary",
+//     artist: "MIMI",
+//     cover: "https://i1.sndcdn.com/artworks-000136774690-7q1q1a-t500x500.jpg",
+//     file: marshmary,
+//     duration: 213,
+//   },
+//   {
+//     title: "Rococo",
+//     artist: "MIMI",
+//     cover: "https://i1.sndcdn.com/artworks-000136774690-7q1q1a-t500x500.jpg",
+//     file: rococo,
+//     duration: 219,
+//   },
+//   {
+//     title: "Sukima",
+//     artist: "MIMI",
+//     cover: "https://i1.sndcdn.com/artworks-000136774690-7q1q1a-t500x500.jpg",
+//     file: sukima,
+//     duration: 219,
+//   },
+// ];
+
+
+import ConMovA from "./CON_MOV_A.mp3";
+import ConMovB from "./CON_MOV_B.mp3";
+import OrgMovA from "./ORG_MOV_A.mp3";
+import OrgMovB from "./ORG_MOV_B.mp3";
+import ConClassicA from "./CON_CLASSIC_A.mp3";
+import OrgClassicA from "./ORG_CLASSIC_A.mp3";
+
+export default [ConMovA, ConMovB, OrgMovA, OrgMovB, ConClassicA, OrgClassicA];
+
+export const music = [
+  {
+    title: "Mode A (Con)",
+    artist: "Theater MOV Sample",
+    cover: "https://i1.sndcdn.com/artworks-000136774690-7q1q1a-t500x500.jpg",
+    file: ConMovA,
+    duration: 28,
+  },
+  {
+    title: "Mode B (Con)",
+    artist: "Theater MOV Sample",
+    cover: "https://i1.sndcdn.com/artworks-000136774690-7q1q1a-t500x500.jpg",
+    file: ConMovB,
+    duration: 30,
+  },
+  {
+    title: "Mode A (Org)",
+    artist: "Theater MOV Sample",
+    cover: "https://i1.sndcdn.com/artworks-000136774690-7q1q1a-t500x500.jpg",
+    file: OrgMovA,
+    duration: 30,
+  },
+  {
+    title: "Mode B (Org)",
+    artist: "Theater MOV Sample",
+    cover: "https://i1.sndcdn.com/artworks-000136774690-7q1q1a-t500x500.jpg",
+    file: OrgMovB,
+    duration: 25,
+  },
+  {
+    title: "Mode A (Con)",
+    artist: "Concert Classical Sample",
+    cover: "https://i1.sndcdn.com/artworks-000136774690-7q1q1a-t500x500.jpg",
+    file: ConClassicA,
+    duration: 66,
+  },
+  {
+    title: "Mode A (Org)",
+    artist: "Concert Classical Sample",
+    cover: "https://i1.sndcdn.com/artworks-000136774690-7q1q1a-t500x500.jpg",
+    file: OrgClassicA,
+    duration: 58,
+  },
+];
